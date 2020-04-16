83faff311c72ad761f3ad56f9a56795d8654a733
Laura Caroline Ribeiro de Melo <lc@ascora.de>
2020-04-16 15:08:37 +0200

    Add Search function to Student list page

--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { StudentsState } from '../store/students/students.reducer';
 import { untilDestroy } from '@ngrx-utils/store';
-import { tap } from 'rxjs/operators';
+import { tap, takeUntil } from 'rxjs/operators';
 import { getAllStudents } from '../store/students/students.selectors';
 import { LoadStudentsRequested } from '../store/students/students.actions';
 import { Aluno } from '../rest-api';
 import { Router } from '@angular/router';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-students',
@@ -22,7 +23,10 @@ export class StudentsComponent implements OnInit, OnDestroy {
     'celular',
     'turmaAtual',
   ];
-  dataSource$ = this.store.pipe(select(getAllStudents));
+  filteredStudentList: Aluno[];
+  studentList: Aluno[];
+
+  searchParameter = new FormControl('');
 
   goToNovoAluno() {
     this.router.navigate(['novoAluno']);
@@ -32,6 +36,43 @@ export class StudentsComponent implements OnInit, OnDestroy {
 
   ngOnInit(): void {
     this.store.dispatch(new LoadStudentsRequested());
+
+    this.store
+      .pipe(
+        untilDestroy(this),
+        select(getAllStudents),
+        tap((students) => {
+          this.studentList = students.slice();
+          this.filterStudentList(this.searchParameter.value);
+        })
+      )
+      .subscribe();
+
+    this.searchParameter.valueChanges.subscribe((value) =>
+      this.filterStudentList(value)
+    );
+  }
+
+  filterStudentList(criteria: string) {
+    criteria = criteria.toLowerCase();
+
+    console.log(criteria, this.studentList, this.filteredStudentList);
+
+    if (!this.studentList) {
+      this.filteredStudentList = [];
+      return;
+    }
+
+    if (!criteria) {
+      this.filteredStudentList = this.studentList.slice();
+      return;
+    }
+
+    this.filteredStudentList = this.studentList.filter(
+      (student) =>
+        student.nome?.toLowerCase().includes(criteria) ||
+        student.CPF?.toString().includes(criteria)
+    );
   }
 
   ngOnDestroy() {

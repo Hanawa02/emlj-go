368f93f45416ed90de8495a9cb449378b1fca162
Laura Caroline Ribeiro de Melo <lc@ascora.de>
2020-04-23 20:30:21 +0200

    Fix auth guard

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -9,8 +9,9 @@ import { select, Store } from '@ngrx/store';
 import { Logout } from '../../store/auth/auth.actions';
 import { AuthState } from '../../store/auth/auth.reducer';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { tap, take } from 'rxjs/operators';
 import { getIsLoggedIn } from 'src/app/store/auth/auth.selectors';
+import { SetIsLoading } from 'src/app/store/core/core.actions';
 
 @Injectable({
   providedIn: 'root',
@@ -18,18 +19,29 @@ import { getIsLoggedIn } from 'src/app/store/auth/auth.selectors';
 export class AuthGuard implements CanActivate {
   constructor(private store: Store<AuthState>, private router: Router) {}
 
-  canActivate(
+  async canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ): Observable<boolean> {
-    return this.store.pipe(
-      select(getIsLoggedIn),
-      tap((loggedIn) => {
-        if (!loggedIn) {
-          this.router.navigate(['login']);
-          this.store.dispatch(new Logout());
-        }
-      })
-    );
+  ): Promise<boolean> {
+    this.store.dispatch(new SetIsLoading(true));
+
+    let result;
+    await this.store
+      .pipe(
+        take(1),
+        select(getIsLoggedIn),
+        tap((loggedIn) => {
+          result = loggedIn;
+          if (!loggedIn) {
+            // this.router.navigate(['login']);
+            this.store.dispatch(new Logout());
+          }
+        })
+      )
+      .subscribe();
+
+    this.store.dispatch(new SetIsLoading(false));
+
+    return result;
   }
 }

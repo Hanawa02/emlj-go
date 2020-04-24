4da6ef0f20b63f4b6df453c0ed178217fd0883c9
Laura Caroline Ribeiro de Melo <lc@ascora.de>
2020-04-24 20:22:48 +0200

    Create login.guard.ts

---
+++ src/app/auth/guards/login.guard.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  RouterStateSnapshot,
+  Router,
+} from '@angular/router';
+import { select, Store } from '@ngrx/store';
+import { Logout } from '../../store/auth/auth.actions';
+import { AuthState } from '../../store/auth/auth.reducer';
+import { Observable } from 'rxjs';
+import { tap, take } from 'rxjs/operators';
+import { getIsLoggedIn } from 'src/app/store/auth/auth.selectors';
+import { SetIsLoading } from 'src/app/store/core/core.actions';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginGuard implements CanActivate {
+  constructor(private store: Store<AuthState>, private router: Router) {}
+
+  async canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Promise<boolean> {
+    let result;
+    await this.store
+      .pipe(
+        take(1),
+        select(getIsLoggedIn),
+        tap((loggedIn) => {
+          result = !loggedIn;
+          if (loggedIn) {
+            this.router.navigate(['alunos']);
+          }
+        })
+      )
+      .subscribe();
+
+    return result;
+  }
+}
